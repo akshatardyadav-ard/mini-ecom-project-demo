d1c35f2c13e400bd027a0c3aeb9257f496c12726
akshatardyadav-ard <akshatardyadav@gmail.com>
2026-01-05 23:15:07 +0530

    again updated db file

--- src/config/db.js
+++ src/config/db.js
@@ -17,23 +17,16 @@
 // });
 
 // module.exports = db;
+require("dotenv").config();
 
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("DB Connection Failed", err);
-  } else {
-    console.log("MySQL Connected");
-  }
 });
 
 module.exports = db;
