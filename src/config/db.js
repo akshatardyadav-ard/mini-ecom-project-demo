ff5b4186d895812fb849a5fc86d65656e692d026
akshatardyadav-ard <akshatardyadav@gmail.com>
2026-01-07 15:51:04 +0530

    created getUser api for railway db's testing

--- src/config/db.js
+++ src/config/db.js
@@ -18,36 +18,22 @@
 
 // module.exports = db;
 
-require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 
   ssl: {
     rejectUnauthorized: false,
   },
 
-  connectTimeout: 20000,
+  waitForConnections: true,
+  connectionLimit: 10,
+  connectTimeout: 30000,
 });
-
-// Optional: test connection once (SAFE)
-(async () => {
-  try {
-    const connection = await pool.getConnection();
-    console.log("✅ MySQL Pool connected");
-    connection.release();
-  } catch (err) {
-    console.error("❌ MySQL Pool connection failed:", err.message);
-  }
-})();
 
 module.exports = pool;
