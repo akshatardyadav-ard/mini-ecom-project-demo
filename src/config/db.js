c0b1d9ed5e0d9b8acf1bbe5cd4680a06d0d6e4fd
akshatardyadav-ard <akshatardyadav@gmail.com>
2026-01-07 14:19:59 +0530

    updated db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,32 +1,19 @@
-// require("dotenv").config();
-// const mysql = require("mysql2");
-
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Database connection failed:", err);
-//     return;
-//   }
-//   console.log("MySQL connected successfully");
-// });
-
-// module.exports = db;
 require("dotenv").config();
+const mysql = require("mysql2");
 
-const mysql = require("mysql2/promise");
-
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+  console.log("MySQL connected successfully");
 });
 
 module.exports = db;
