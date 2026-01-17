3b6e7cddd183a11b6527accceb192e905c7c6190
akshatardyadav-ard <akshatardyadav@gmail.com>
2026-01-17 14:31:40 +0530

    deleted the frontend folder

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: CheckoutComponent
-    }
-];
